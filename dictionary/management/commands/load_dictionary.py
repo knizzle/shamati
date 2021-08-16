c75c959c97038b641f3b93f95858de6c74d2d502
Kelsey <klsycanoy@yahoo.com>
2021-08-16 09:35:03 -0700

    minimal progress since 8/13

--- dictionary/management/commands/load_dictionary.py
+++ dictionary/management/commands/load_dictionary.py
@@ -1,30 +1,30 @@
   
-# import json
+import json
 
-# from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand
 
-# from ...models import Phonemes, HebWord
+from ...models import Phonemes, HebWord
 
-# class Command(BaseCommand):
+class Command(BaseCommand):
 
-#     def handle(self, *args, **options):
+    def handle(self, *args, **options):
         
-#         # Clear existing Pokemon and Types
-#         Phonemes.objects.all().delete()
-#         HebWord.objects.all().delete()
+        # Clear existing Phonemes and HebWords
+        Phonemes.objects.all().delete()
+        HebWord.objects.all().delete()
 
-#         # Open Pokemon JSON and load to list of dictionaries
-#         with open('phonemes.json') as p:
-#             phonemes = json.loads(p.read())
+        # Open phonemes JSON and load to list of dictionaries
+        with open('phonemes.json') as p:
+            phonemes = json.loads(p.read())
         
-#         # Loop through Pokemon to add to DB
-#         for phoneme in phonemes:
+        # Loop through Phonemes to add to DB
+        for phoneme in phonemes:
 
 #             # Convert units to m and kg
 #             pokemon['height'] /= 10
 #             pokemon['weight'] /= 10
 
-#             # Add Pokemon to DB
+#             # Add Phonemes to DB
 #             poke_obj = Pokemon.objects.create(
 #                 number=pokemon['number'],
 #                 name=pokemon['name'],
@@ -34,10 +34,7 @@
 #                 image_back=pokemon['image_back']
 #             )
 
-#             # Loop through types list
-#             # For each type, create type if it doesn't exist yet, than add that type to current Pokemon
-#             for type in pokemon['types']:
-#                 type_obj, created = Type.objects.get_or_create(type=type)
+#             # Loop through HebWord list
 #                 type_obj.pokemon.add(poke_obj)
 
 
@@ -45,4 +42,4 @@
 # with above step: confirm your models look good -- modify any fields, re-makemigrations, migrate if needed
 # once your DB looks good... work on api_app serializers.py and views.py...
 # ... until the automatic REST /api/v1/ interface looks correct
-# then get stuff displayed in Vue on front end
+# then get stuff displayed in Vue on front-end
