4f261829066e122aed9f50e7c0352af0d4011076
Kelsey <klsycanoy@yahoo.com>
2021-08-21 09:25:35 -0700

    algorithm progress

---
+++ algorithm.py
@@ -0,0 +1,55 @@
+import itertools
+
+dictionary = {
+    "א": ["", "a", "'", "o", "i"],
+    "ב": ["v", "b"],
+    "ג": ["g", "j", "zh"],
+    "ד": ["d"],
+    "ה": ["", "h", "eh", "ah", "a", "e", "ay", "ei", "ai", "ey"],
+    "ו": ["v", "w", "oh", "o", "wu", "oo", "u", "ou", "oe", "ue"],
+    "ז": ["z", "s"],
+    "ח": ["ch", "h", "kh", "ach", "akh"],
+    "ט": ["t", "th"],
+    "י": ["y", "i", ""],
+    "כ": ["ch", "h", "k", "c", "kh", "ck"],
+    "ל": ["l"],
+    "מ": ["m"],
+    "נ": ["n"],
+    "ס": ["s"],
+    "ע": ["", "a", "i", "ee"],
+    "פ": ["f", "ph", "p"],
+    "צ": ["ts", "tz", "s", "z", "izz", "tez"],
+    "ק": ["k", "c", "q"],
+    "ר": ["r"],
+    "ש": ["s", "sh", "ch", "sch"],
+    "ת": ["t", "th"],
+    "יי": ["ai", "ay", "ey", "ei"],
+    "יו": ["av", "ev"],
+    "וו": ["w", "wu", "wa", "wo", "v", "ve", "va"],
+    "": ["a", "i", "e", "o", "h", "u", "y"]
+}
+
+user_entry = 'rosh'
+
+def combo_objs(user_entry):
+    word = [user_entry[i:j] for i, j in itertools.combinations(range(len(user_entry)+1),2)]
+    combos = sorted(word, key=len, reverse=True)
+    return combos
+
+def last_letter(user_entry): 
+    permutations = []
+    for letter in user_entry:
+        keys = [key for key, value in dictionary.items() if letter in value]
+        permutations.append(keys)   
+    if permutations[len(user_entry)-1] == ['מ']:
+        return 'ם'
+    elif permutations[len(user_entry)-1] == ['פ']:
+        return 'ף'
+    elif permutations[len(user_entry)-1] == ['כ']:
+        return 'ך'
+    elif permutations[len(user_entry)-1] == ['צ']:
+        return 'ץ'
+    elif permutations[len(user_entry)-1] == ['נ']:
+        return 'ן'
+    else:
+        return -1
