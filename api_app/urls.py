50f910f0f198623b9d08834b008dc38131271ee2
Kelsey <klsycanoy@yahoo.com>
2021-08-26 14:01:27 -0700

    word match in progress

--- api_app/urls.py
+++ api_app/urls.py
@@ -1,19 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import HebString
-
-# from .views import DictionaryViewSet, SoundsViewSet, LettersViewSet
+from .views import HebString, HebWords
 
 router = DefaultRouter()
-# router.register('hebwords', DictionaryViewSet, basename='words')
-# router.register('phonemes', SoundsViewSet, basename='sounds')
-# router.register('hebletters', LettersViewSet, basename='letters')
-
-# urlpatterns = router.urls
 
 urlpatterns = [
     path('hebstring/lookup/', HebString.as_view()),
     path('hebstring/<str:input_word>/', HebString.as_view()),
-    # path('api_app/', HebString.as_view(), name='home.html')
+    path('hebwords/', HebWords.as_view())
 ]
--- api_app/views.py
+++ api_app/views.py
@@ -8,11 +8,7 @@ import itertools
 
 class HebString(APIView):
 
-    def get(self, request, format=None):
-        # build up list of possible hebrew letters for each phoneme, see heb_keys func.
-        # generate combinations of hebrew letters, see itertools example
-        # check if each hebrew letter is in the database (check if its a real word)
-        # return a list of hebrew words
+    def get(self):
         return Response()
         
     def post(self, request, format=None):
@@ -38,3 +34,35 @@ class HebString(APIView):
             results_list.append(result)
             print(result)
         return Response(results_list)
+
+class HebWords(APIView):       
+    # check if each hebrew letter is in the database (check if it's a real word)
+    # return a list of hebrew words
+
+    def get(self):
+        return Response()
+        
+    def post(self, request, format=None):
+        heb_list = []
+        input_word = request.data['phonemes']
+        print(input_word)
+        for input_letter in input_word:
+            phoneme = Phonemes.objects.filter(text=input_letter).first()
+            if phoneme is None:
+                print('phoneme not found')
+                continue
+            queryset_of_hebrew_letter__matches = phoneme.hebletter.all()
+            heb_letters = []
+            for hebletter in queryset_of_hebrew_letter__matches:
+                heb_letters.append(hebletter.letter)
+            heb_list.append(heb_letters)
+        results_list = []
+        print(heb_list)
+        results = list(itertools.product(*heb_list))
+        print(results)
+        for result in results:
+            result = ''.join(result)
+            results_list.append(result)
+            print(result)
+        if result == HebWord.objects.filter():
+            return Response(result)
